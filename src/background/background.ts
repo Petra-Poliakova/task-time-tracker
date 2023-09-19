4303b5f489b80f27ac3e66d86da3a85c42f1d46e
Petra poliakova <petra.poliakova@dxc.com>
2023-09-19 16:08:41 +0200

    inject content with background

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,9 +3,25 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 });
 
+try{
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if(changeInfo.status === 'complete' && tab.id !== undefined) {
+        chrome.scripting.executeScript({
+            files: ['injectContent.js'],
+            target: {tabId: tab.id as number},
+        })
+    }
+}); 
+}catch(e){
+    console.log(e);
+  }
 // chrome.storage.local.get(['activeTask'], (res) => {
 //     chrome.storage.local.set({
 //         activeTask: "activeTask" in res ? res.activeTask : []
 //     })
 
 // })
+
+//https://www.youtube.com/watch?v=qwEFy4FTbNY&list=PLxOYpovHh6ISG4dqNfckBiuoT2eXMC5iU
+//https://www.youtube.com/watch?v=tOHpufRbsJc
+//https://www.youtube.com/watch?v=y3FF1QxKu5E
--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,13 +1,11 @@
-import './contentScript.css'
-
 chrome.runtime.sendMessage('From the content script!', (response)=> {
     console.log(response)
 } );
 
-const init = () => {
-    const injectElement = document.createElement('div');
-    injectElement.className = 'rustyZone-element';
-    injectElement.innerHTML = 'Hello from the Rusty Zone Element';
-    document.body.appendChild(injectElement);
-}
-init();
+// const init = () => {
+//     const injectElement = document.createElement('div');
+//     injectElement.className = 'rustyZone-element';
+//     injectElement.innerHTML = 'Hello from the Rusty Zone Element';
+//     document.body.appendChild(injectElement);
+// }
+// init();

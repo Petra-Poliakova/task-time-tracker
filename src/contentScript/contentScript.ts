3d5039bb6588a1bda31bcfde0cbe8df521db6223
Petra poliakova <petra.poliakova@dxc.com>
2023-09-19 14:37:39 +0200

    inject content with contentScript

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,3 +1,13 @@
+import './contentScript.css'
+
 chrome.runtime.sendMessage('From the content script!', (response)=> {
     console.log(response)
-} )
+} );
+
+const init = () => {
+    const injectElement = document.createElement('div');
+    injectElement.className = 'rustyZone-element';
+    injectElement.innerHTML = 'Hello from the Rusty Zone Element';
+    document.body.appendChild(injectElement);
+}
+init();
